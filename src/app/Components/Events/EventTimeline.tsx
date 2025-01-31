cd5874220297e61e28af7b391cc27aba157097ea
Aditya <adityamvs202@gmail.com>
2025-02-01 02:01:12 +0530

    added a animation in events

--- src/app/Components/Events/EventTimeline.tsx
+++ src/app/Components/Events/EventTimeline.tsx
@@ -1,8 +1,24 @@
 "use client";
+
 import Image from "next/image";
 import React, { useEffect, useId, useRef, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
-import { useOutsideClick } from "@/app/Components/hooks/use-outside-click";
+
+// Custom hook to detect clicks outside an element
+const useOutsideClick = (ref: React.RefObject<HTMLElement>, callback: () => void) => {
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        callback();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref, callback]);
+};
 
 export default function EventTimeline() {
   const [active, setActive] = useState<(typeof cards)[number] | boolean | null>(
@@ -10,6 +26,9 @@ export default function EventTimeline() {
   );
   const ref = useRef<HTMLDivElement>(null);
   const id = useId();
+
+  // Use the useOutsideClick hook to close the modal when clicking outside
+  useOutsideClick(ref, () => setActive(null));
 
   useEffect(() => {
     function onKeyDown(event: KeyboardEvent) {
@@ -28,14 +47,17 @@ export default function EventTimeline() {
     return () => window.removeEventListener("keydown", onKeyDown);
   }, [active]);
 
-  useOutsideClick(ref, () => setActive(null));
-
   return (
     <>
-    <div className="h-30vw py-20 w-full ">
-              <h4 className='bg-clip-text text-transparent bg-gradient-to-b from-red-200 to-red-800 bg-opacity-50 font-bold md:text-[60px] sm:text-[50px] text-center xs:text-[40px] text-[30px] animate-popIn' style={{fontFamily:'Nasalization, sans-serif'}}>Explore our Events! </h4>
-    
-        </div>
+      <div className="h-30vw py-20 w-full">
+        <h4
+          className="bg-clip-text text-transparent bg-gradient-to-b from-red-200 to-red-800 bg-opacity-50 font-bold md:text-[60px] sm:text-[50px] text-center xs:text-[40px] text-[30px] animate-popIn"
+          style={{ fontFamily: "Nasalization, sans-serif" }}
+        >
+          Explore our Events!
+        </h4>
+      </div>
+
       <AnimatePresence>
         {active && typeof active === "object" && (
           <motion.div
@@ -46,24 +68,16 @@ export default function EventTimeline() {
           />
         )}
       </AnimatePresence>
+
       <AnimatePresence>
         {active && typeof active === "object" ? (
-          <div className="fixed inset-0  grid place-items-center z-[100]">
+          <div className="fixed inset-0 grid place-items-center z-[100]">
             <motion.button
               key={`button-${active.title}-${id}`}
               layout
-              initial={{
-                opacity: 0,
-              }}
-              animate={{
-                opacity: 1,
-              }}
-              exit={{
-                opacity: 0,
-                transition: {
-                  duration: 0.05,
-                },
-              }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0, transition: { duration: 0.05 } }}
               className="flex absolute top-2 right-2 lg:hidden items-center justify-center bg-white rounded-full h-6 w-6"
               onClick={() => setActive(null)}
             >
@@ -72,7 +86,7 @@ export default function EventTimeline() {
             <motion.div
               layoutId={`card-${active.title}-${id}`}
               ref={ref}
-              className="w-full max-w-[500px]  h-full md:h-fit md:max-h-[90%]  flex flex-col bg-white dark:bg-neutral-900 sm:rounded-3xl overflow-hidden"
+              className="w-full max-w-[500px] h-full md:h-fit md:max-h-[90%] flex flex-col bg-white dark:bg-neutral-900 sm:rounded-3xl overflow-hidden"
             >
               <motion.div layoutId={`image-${active.title}-${id}`}>
                 <Image
@@ -87,7 +101,7 @@ export default function EventTimeline() {
 
               <div>
                 <div className="flex justify-between items-start p-4">
-                  <div className="">
+                  <div>
                     <motion.h3
                       layoutId={`title-${active.title}-${id}`}
                       className="font-bold text-neutral-700 dark:text-neutral-200"
@@ -129,67 +143,110 @@ export default function EventTimeline() {
           </div>
         ) : null}
       </AnimatePresence>
-      <ul className="max-w-2xl mx-auto w-full gap-4">
-        {cards.map((card) => (
-          <motion.div
-            layoutId={`card-${card.title}-${id}`}
+
+      <div className="max-w-2xl mx-auto w-full">
+        {cards.map((card, index) => (
+          <TimelineCard
             key={`card-${card.title}-${id}`}
-            onClick={() => setActive(card)}
-            className="p-4 flex flex-col md:flex-row justify-between items-center hover:bg-neutral-50 dark:hover:bg-neutral-800 rounded-xl cursor-pointer"
-          >
-            <div className="flex gap-4 flex-col md:flex-row ">
-              <motion.div layoutId={`image-${card.title}-${id}`}>
-                <Image
-                  width={100}
-                  height={100}
-                  src={card.src}
-                  alt={card.title}
-                  className="h-40 w-40 md:h-14 md:w-14 rounded-lg object-cover object-top"
-                />
-              </motion.div>
-              <div className="">
-                <motion.h3
-                  layoutId={`title-${card.title}-${id}`}
-                  className="font-bold text-neutral-800 dark:text-neutral-200 text-center md:text-left"
-                >
-                  {card.title}
-                </motion.h3>
-                <motion.p
-                  layoutId={`description-${card.description}-${id}`}
-                  className="text-neutral-600 dark:text-neutral-400 text-center md:text-left"
-                >
-                  {card.description}
-                </motion.p>
-              </div>
-            </div>
-            <motion.button
-              layoutId={`button-${card.title}-${id}`}
-              className="relative overflow-hidden px-4 py-2 text-xl rounded-full font-bold text-black bg-gray-100 mt-4 md:mt-0 group hover:text-white hover:bg-transparent">
-              <span className="absolute inset-0 bg-gradient-to-r from-red-500 to-red-800 transform scale-x-0 group-hover:scale-x-100 opacity-0 group-hover:opacity-100 transition-all duration-1000 ease-in-out z-0 animate-liquid"></span>
-              <span className="relative z-10">Register</span>
-            </motion.button>
-          </motion.div>
+            card={card}
+            index={index}
+            setActive={setActive}
+            id={id}
+          />
         ))}
-      </ul>
+      </div>
     </>
   );
 }
 
+const TimelineCard = ({
+  card,
+  index,
+  setActive,
+  id,
+}: {
+  card: (typeof cards)[number];
+  index: number;
+  setActive: (card: (typeof cards)[number]) => void;
+  id: string;
+}) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.unobserve(entry.target);
+        }
+      },
+      { threshold: 0.5 }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
+
+  return (
+    <motion.div
+      ref={ref}
+      layoutId={`card-${card.title}-${id}`}
+      onClick={() => setActive(card)}
+      className="p-4 flex flex-col md:flex-row justify-between items-center hover:bg-neutral-50 dark:hover:bg-neutral-800 rounded-xl cursor-pointer"
+      initial={{ opacity: 0, y: 50 }}
+      animate={isVisible ? { opacity: 1, y: 0 } : {}}
+      transition={{ duration: 0.5, delay: index * 0.2 }}
+    >
+      <div className="flex gap-4 flex-col md:flex-row">
+        <motion.div layoutId={`image-${card.title}-${id}`}>
+          <Image
+            width={100}
+            height={100}
+            src={card.src}
+            alt={card.title}
+            className="h-40 w-40 md:h-14 md:w-14 rounded-lg object-cover object-top"
+          />
+        </motion.div>
+        <div>
+          <motion.h3
+            layoutId={`title-${card.title}-${id}`}
+            className="font-bold text-neutral-800 dark:text-neutral-200 text-center md:text-left"
+          >
+            {card.title}
+          </motion.h3>
+          <motion.p
+            layoutId={`description-${card.description}-${id}`}
+            className="text-neutral-600 dark:text-neutral-400 text-center md:text-left"
+          >
+            {card.description}
+          </motion.p>
+        </div>
+      </div>
+      <motion.button
+        layoutId={`button-${card.title}-${id}`}
+        className="relative overflow-hidden px-4 py-2 text-xl rounded-full font-bold text-black bg-gray-100 mt-4 md:mt-0 group hover:text-white hover:bg-transparent"
+      >
+        <span className="absolute inset-0 bg-gradient-to-r from-red-500 to-red-800 transform scale-x-0 group-hover:scale-x-100 opacity-0 group-hover:opacity-100 transition-all duration-1000 ease-in-out z-0 animate-liquid"></span>
+        <span className="relative z-10">Register</span>
+      </motion.button>
+    </motion.div>
+  );
+};
+
 export const CloseIcon = () => {
   return (
     <motion.svg
-      initial={{
-        opacity: 0,
-      }}
-      animate={{
-        opacity: 1,
-      }}
-      exit={{
-        opacity: 0,
-        transition: {
-          duration: 0.05,
-        },
-      }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0, transition: { duration: 0.05 } }}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -219,10 +276,9 @@ const cards = [
       return (
         <p>
           The Entertainment Quiz is a fun-filled challenge that dives into the
-world of movies, music, and pop culture. Test your knowledge and
-compete with others to prove you&apos;re the ultimate entertainment
-enthusiast!
-
+          world of movies, music, and pop culture. Test your knowledge and
+          compete with others to prove you&apos;re the ultimate entertainment
+          enthusiast!
         </p>
       );
     },
@@ -236,10 +292,10 @@ enthusiast!
     content: () => {
       return (
         <p>
-         The Youth Parliament is a platform for young minds to debate, discuss,
-and deliberate on pressing global and national issues. It empowers
-participants to voice their perspectives, fostering leadership and critical
-thinking skills.
+          The Youth Parliament is a platform for young minds to debate, discuss,
+          and deliberate on pressing global and national issues. It empowers
+          participants to voice their perspectives, fostering leadership and
+          critical thinking skills.
         </p>
       );
     },
@@ -253,16 +309,14 @@ thinking skills.
     content: () => {
       return (
         <p>
-         The podcast series brings insightful conversations with experts, thought
+          The podcast series brings insightful conversations with experts, thought
           leaders, and influencers, covering the latest trends in technology,
-        entertainment, and culture. Tune in for discussions that spark curiosity
-and broaden perspectives
+          entertainment, and culture. Tune in for discussions that spark curiosity
+          and broaden perspectives.
         </p>
       );
     },
   },
-
- 
   {
     description: "Gaming Event",
     title: "ESPORTS SAGA",
@@ -273,16 +327,15 @@ and broaden perspectives
       return (
         <p>
           Esports Saga is a competitive gaming event that brings together players
-to showcase their skills in a series of popular gaming tournaments.
-Participants compete in an engaging and dynamic environment,
-celebrating the exciting world of e-sports.
-
+          to showcase their skills in a series of popular gaming tournaments.
+          Participants compete in an engaging and dynamic environment,
+          celebrating the exciting world of e-sports.
         </p>
       );
     },
   },
   {
-    description: "IDEATHON",
+    description: "Ideathon",
     title: "IDEATHON",
     src: "https://i.ibb.co/qDn3ZsM/ideathon4.jpg",
     ctaText: "Register",
@@ -291,18 +344,18 @@ celebrating the exciting world of e-sports.
       return (
         <p>
           Ideathon is an exciting inter-departmental event designed to foster
-collaboration and innovation among students from across the college.
-Participants will form groups of three or four, created randomly, and
-work together to brainstorm a unique and innovative startup idea. The
-event promotes the exchange of ideas and perspectives, encouraging
-creativity and teamwork
+          collaboration and innovation among students from across the college.
+          Participants will form groups of three or four, created randomly, and
+          work together to brainstorm a unique and innovative startup idea. The
+          event promotes the exchange of ideas and perspectives, encouraging
+          creativity and teamwork.
         </p>
       );
     },
   },
   {
     description: "Bootcamp",
-    title: "The DATA DOJO",
+    title: "THE DATA DOJO",
     src: "https://i.ibb.co/mcDbQpR/Data-analytics.jpg",
     ctaText: "Register",
     ctaLink: "https://forms.gle/8z2N73E4J1vibFGXA",
@@ -315,7 +368,6 @@ creativity and teamwork
           and create impactful reports to support decision-making. The session is
           designed to equip attendees with practical skills for solving real-world
           problems in the data analytics domain.
-
         </p>
       );
     },
@@ -337,6 +389,4 @@ creativity and teamwork
       );
     },
   },
-  
-  
 ];
