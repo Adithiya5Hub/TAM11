603e0d34c61c6e5be8c2ae8c762c3da5924cc702
6kumaran <kumaran61003@gmail.com>
2025-02-03 14:21:28 +0530

    Eagle Animation

---
+++ src/app/Components/Eagle.js
@@ -0,0 +1,38 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export default function Eagle({ keepHeadingVisible }) {
+  const [showEagle, setShowEagle] = useState(false);
+
+  useEffect(() => {
+    // Trigger eagle animation
+    setTimeout(() => {
+      setShowEagle(true);
+    }, 1000);
+  }, []);
+
+  return (
+    <div className="container mt-[20vh] pt-[10vh] pb-[10vh] md:pt-[20vh] md:pb-[20vh]">
+      {showEagle && (
+        <img
+          src="https://i.ibb.co/VWk9Mdpm/favicon-removebg-preview.png"
+          alt="Eagle"
+          className="eagle"
+        />
+      )}
+      {keepHeadingVisible && (
+        <div className="heading text-6xl md:text-8xl font-bold text-center fade-in">
+          Technical Awareness Month
+        </div>
+      )}
+    </div>
+  );
+}
+
+
+
+
+
+
+// "heading text-6xl md:text-8xl font-bold text-center"
+// container mt-[20vh] pt-[20vh] pb-[20vh]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,21 @@
 import Navbar from "@/app/Components/Navbar";
 import Faculty from "@/app/Components/Home/Faculty"
 import Footer from "@/app/Components/Home/Footer";
-import HeroSection from "@/app/Components/Home/Overview";
 import OurMission from "./Components/Home/mission";
+import Eagle from "./Components/Eagle"
+import { useEffect, useState } from "react";
 export default function Home() {
+  const [animationFinished, setAnimationFinished] = useState(false);
+
+  useEffect(() => {
+    // Delay content rendering until animation finishes
+    const animationDuration = 4000; // 3s eagle animation + 1s fade-out
+    const timer = setTimeout(() => {
+      setAnimationFinished(true);
+    }, animationDuration);
+
+    return () => clearTimeout(timer); // Cleanup timer
+  }, []);
   return (
     
       <div style={{ fontFamily: 'Striger, sans-serif' }}>
@@ -13,13 +25,20 @@ export default function Home() {
           {/* <TracingBeam className="px-6"> */}
           {/* <TracingBeam className="px-6"> */}
           {/* <TracingBeam className="px-6"> */}
-          <HeroSection/>
+          <Eagle keepHeadingVisible={animationFinished}/>
+          {animationFinished && (
+        <div
+          className={`transition-opacity duration-1000 opacity-0 ${
+            animationFinished ? "opacity-100" : ""
+          } pt-20`}
+        >
           <OurMission />
           <Faculty />
           {/*<Events />*/}
           {/* <FAQ /> */}
           <Footer />
-          
+          </div>
+      )}
         {/* </TracingBeam> */}
         
       </div>
