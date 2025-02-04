d702d14d8194a5f6e4039ad470a54ed52f7e76a4
S. Adithiya <adithiya1805@gmail.com>
2025-02-04 22:20:33 +0530

    Resolved deployment issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,9 @@ import OurMission from "./Components/Home/mission";
 import Overview from "./Components/Home/Overview";
 // import Events from "./Components/Home/Events";
 
-import { useEffect, useState } from "react";
+
 export default function Home() {
-  const [animationFinished, setAnimationFinished] = useState(false);
-
-  useEffect(() => {
-    // Delay content rendering until animation finishes
-    const animationDuration = 4000; // 3s eagle animation + 1s fade-out
-    const timer = setTimeout(() => {
-      setAnimationFinished(true);
-    }, animationDuration);
-
-    return () => clearTimeout(timer); // Cleanup timer
-  }, []);
+  
   return (
     
       <div style={{ fontFamily: 'Striger, sans-serif' }}>
